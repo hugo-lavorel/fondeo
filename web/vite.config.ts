da112434f52f53713358cd841f1bf825df8c74dd
hugo-lavorel <hugo.lavorel@gmail.com>
2026-03-10 20:56:58 +0100

    fix: config for apple silicon

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,16 +1,19 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: true,
+    proxy: {
+      "/api": "http://localhost:3000",
+    },
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
